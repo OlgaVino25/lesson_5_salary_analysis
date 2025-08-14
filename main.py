e1f1648796c754dab1695b03ce088a0aaa9c74b9
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-08-14 14:18:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -34,36 +34,40 @@ def main():
     load_dotenv()
 
     # Настройки для HeadHunter
-    HH_ACCESS_TOKEN = os.getenv('HH_ACCESS_TOKEN')
-    HH_CONTACT_EMAIL = os.getenv('YANDEX_LOGIN')
-    
+    hh_access_token = os.getenv('HH_ACCESS_TOKEN')
+    hh_contact_email = os.getenv('HH_CONTACT_EMAIL')
+
     hh_headers = {
-        'Authorization': f'Bearer {HH_ACCESS_TOKEN}',
-        'User-Agent': f'MyHHIntegration/1.0 ({HH_CONTACT_EMAIL})'
+        'Authorization': f'Bearer {hh_access_token}',
+        'User-Agent': f'MyHHIntegration/1.0 ({hh_contact_email})'
     }
-    
+
     # Настройки для SuperJob
-    SUPER_JOB_SECRET_KEY = os.getenv('SUPER_JOB_SECRET_KEY')
-    SUPER_JOB_TOKEN = os.getenv('SUPER_JOB_TOKEN')
+    super_job_secret_key = os.getenv('SUPER_JOB_SECRET_KEY')
+    super_job_token = os.getenv('SUPER_JOB_TOKEN')
 
     sj_headers = {
-        'X-Api-App-Id': SUPER_JOB_SECRET_KEY,
-        'Authorization': f'Bearer {SUPER_JOB_TOKEN}',
+        'X-Api-App-Id': super_job_secret_key,
+        'Authorization': f'Bearer {super_job_token}',
     }
-    
+
     print('\nСбор статистики с HeadHunter...')
     try:
         hh_stats = get_hh_statistics(city_name, hh_headers, LANGUAGES)
         print_stats_table(hh_stats, city_name, 'HeadHunter')
-    except (requests.exceptions.RequestException, ValueError) as e:
-        print(f'Ошибка при получении данных HeadHunter: {e}')
+    except requests.exceptions.RequestException as e:
+        print(f'Сетевая ошибка при подключении к HeadHunter: {e}')
+    except ValueError as e:
+        print(f'Ошибка данных HeadHunter: {e}')
 
     print('\nСбор статистики с SuperJob...')
     try:
         sj_stats = get_sj_statistics(city_name, sj_headers, LANGUAGES)
         print_stats_table(sj_stats, city_name, 'SuperJob')
-    except (requests.exceptions.RequestException, ValueError) as e:
-        print(f'Ошибка при получении данных SuperJob: {e}')
+    except requests.exceptions.RequestException as e:
+        print(f'Сетевая ошибка при подключении к SuperJob: {e}')
+    except ValueError as e:
+        print(f'Ошибка данных SuperJob: {e}')
 
 
 if __name__ == '__main__':
